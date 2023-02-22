1428af5a1b0cee519cd27bdd3ccf126171c6c687
SajiburMunna <sajiburmunna087@gmail.com>
2023-02-23 00:19:20 +0600

    chore:project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { IBM_Plex_Mono } from "@next/font/google";
+
+import "@/styles/globals.css";
+
+const IBM = IBM_Plex_Mono({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={IBM.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
